d317a1e1624da2e889e05946415176ebe7af163f
tebbini203-lang <tebbini203@gmail.com>
2026-02-10 18:58:15 +0100

    Derniere

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,25 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
+    #[Route('/', name: 'app_home')]
+    public function home()
+    {
+        return $this->render('pages/home.html.twig');
+    }
+
     #[Route('/about', name: 'app_about')]
-    public function about(): Response
+    public function about()
     {
         return $this->render('pages/about.html.twig');
     }
 
     #[Route('/service', name: 'app_service')]
-    public function service(): Response
+    public function service()
     {
         return $this->render('pages/service.html.twig');
     }
 
     #[Route('/contact', name: 'app_contact')]
-    public function contact(): Response
+    public function contact()
     {
         return $this->render('pages/contact.html.twig');
     }
